5885bf4d7a773ed2b908c4e9ab20f4d6d7a4b85c
Kruchy <kruchy61@gmail.com>
2017-07-09 21:32:50 +0200

    Add application properties

--- src/main/java/pl/edu/agh/kis/kruchy/configuration/HibernateConfiguration.java
+++ src/main/java/pl/edu/agh/kis/kruchy/configuration/HibernateConfiguration.java
@@ -1,8 +1,5 @@
 package pl.edu.agh.kis.kruchy.configuration;
 
-import java.util.Properties;
-import javax.sql.DataSource;
-
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +11,9 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Properties;
 
 @Configuration
 @EnableTransactionManagement
@@ -36,18 +36,18 @@ public class HibernateConfiguration {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(environment.getRequiredProperty("jdbc.driverClassName"));
-        dataSource.setUrl(environment.getRequiredProperty("jdbc.url"));
-        dataSource.setUsername(environment.getRequiredProperty("jdbc.username"));
-        dataSource.setPassword(environment.getRequiredProperty("jdbc.password"));
+        dataSource.setDriverClassName(environment.getRequiredProperty("spring.data.jdbc.driverClassName"));
+        dataSource.setUrl(environment.getRequiredProperty("spring.data.hibernate.url"));
+        dataSource.setUsername(environment.getRequiredProperty("spring.data.hibernate.username"));
+        dataSource.setPassword(environment.getRequiredProperty("spring.data.hibernate.password"));
         return dataSource;
     }
 
     private Properties hibernateProperties() {
         Properties properties = new Properties();
-        properties.put("hibernate.dialect", environment.getRequiredProperty("hibernate.dialect"));
-        properties.put("hibernate.show_sql", environment.getRequiredProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getRequiredProperty("hibernate.format_sql"));
+        properties.put("hibernate.dialect", environment.getRequiredProperty("spring.data.hibernate.dialect"));
+        properties.put("hibernate.show_sql", environment.getRequiredProperty("spring.data.hibernate.show_sql"));
+        properties.put("hibernate.format_sql", environment.getRequiredProperty("spring.data.hibernate.format_sql"));
         return properties;
     }
 
