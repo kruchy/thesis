255fb345371d82ffec48185cc0748eab31d315e0
Kruchy <kruchy61@gmail.com>
2017-07-17 14:06:06 +0200

    Add main

---
+++ prevayler/src/main/java/pl/edu/agh/kis/kruchy/prevayler/MainApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.agh.kis.kruchy.prevayler;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+
+
+    public static void main(String[] args) {
+
+    }
+}
